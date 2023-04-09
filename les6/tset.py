52152ff7e37315f0fbe2674fa525f733605e8c95
GummyDonkey <kpebetkabtankax@gmail.com>
2023-04-09 17:47:26 +0500

    l6fin

---
+++ les6/tset.py
@@ -0,0 +1,10 @@
+def sum_dividers(n):
+    return sum(x for x in range(1, n//2+1) if not n % x)
+
+
+k = int(input('input k: '))
+
+for i in range(1, k+1):
+    potentially_friendly = sum_dividers(i)
+    if i < potentially_friendly and i == sum_dividers(potentially_friendly):
+        print(i, potentially_friendly)
